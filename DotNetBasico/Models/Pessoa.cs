19454293c174b8fb177b9a840ff59ddbaddeb328
matheus <matheus@novetech.com.br>
2023-12-29 19:12:39 -0300

    Proj Basico

---
+++ DotNetBasico/Models/Pessoa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetBasico.Models
+{
+    public class Pessoa
+    {
+        private string _nome;
+        public string Nome 
+        {
+            get
+            {
+                return _nome.ToUpper();
+            }
+            set 
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("NOme não pode ser vazio");
+                }
+                _nome = value;
+            } 
+        }
+        public int Idade { get; set; }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine($"Nome da Pessoa é: {_nome}, tem {Idade} anos de Idade");
+        }
+    }
+}
---
+++ DotNetBasico/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using DotNetBasico.Models;
+
+Pessoa pessoa = new Pessoa();
+pessoa.Nome = "Burumungu";
+pessoa.Idade = 40;
+pessoa.ExibirDados();
